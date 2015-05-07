811542bbcd543aea82ad2429d8c45aa43b25fba5
etlima <etlima@gmail.com>
2015-05-07 15:53:58 -0300

    Update wmi free space disco c.ps1

--- wmi free space disco c.ps1
+++ wmi free space disco c.ps1
@@ -3,3 +3,4 @@ Get-WmiObject -Class Win32_LogicalDisk `
 -ComputerName $_ -Filter "DeviceId='C:'" } |
 Format-Table SystemName, @{Name="Free";
 Expression={[math]::round($($_.FreeSpace/1GB), 2)}} -auto
+# new line
