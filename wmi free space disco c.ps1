2e21bf987bb96787d17ef7daa13a62996b0f48a9
etlima <etlima@gmail.com>
2015-05-07 15:19:57 -0300

    Create wmi free space disco c.ps1

---
+++ wmi free space disco c.ps1
@@ -0,0 +1,5 @@
+"hlmmanager", "hlmirs-01", "DTCVEX-MBX-01"| foreach {
+Get-WmiObject -Class Win32_LogicalDisk `
+-ComputerName $_ -Filter "DeviceId='C:'" } |
+Format-Table SystemName, @{Name="Free";
+Expression={[math]::round($($_.FreeSpace/1GB), 2)}} -auto
